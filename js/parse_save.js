4195ec491ea92acbcdf9b08909099c3c94abd52b
Xuhao Wang <wxuhao@gmail.com>
2021-04-01 14:20:51 -0400

    parse

--- js/parse_save.js
+++ js/parse_save.js
@@ -1,14 +1,24 @@
 function parseFile(file) {
     const reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
         var $xml = $.parseXML(e.target.result);
         $save = $($xml);
         $save = $save.find('SaveGame');
-        // find player
-        //console.log($($xml, 'player'));
-        console.log($save);
-        console.log(parsePathList(['player'], $save));
-        console.log(parsePathList(['fdsa'], $save));
+        // Dict of item: [path, to, item]
+        paths = {
+            playerName: ['player', 'name'],
+            friendNames: ['friendshipData', 'string'],
+            friendPoints: ['friendshipData', 'Friendship', 'Points'],
+        };
+        // Final values after parsing
+        info = {};
+        // Parse each thing in paths
+        $.each(paths, function(key, value) {
+            info[key] = parseEach(value, $save);
+        })
+        // Edit weird values
+        info.playerName = [info.playerName[0]]
+        console.log(info);
     }
     reader.onerror = error => reject(error);
     reader.readAsText(file);
@@ -16,11 +26,11 @@ function parseFile(file) {
 
 function parsePathList(list, $save) {
     //successively .find each entry in the list and return the last piece of xml
-    $.each(list, function(index, value) {
+    $.each(list, function (index, value) {
         $temp_save = $save.find(value);
         // Doesn't throw a clear error?
-        if ($temp_save.length === 0){
-            throw('Could not find ' + value + ' in ' + $save);
+        if ($temp_save.length === 0) {
+            throw ('Could not find ' + value + ' in ' + $save);
         }
         $save = $temp_save;
     });
@@ -28,5 +38,9 @@ function parsePathList(list, $save) {
 }
 
 function parseEach(list, $save) {
-    //parsePathList for each item found with .find
+    values = [];
+    $.each(parsePathList(list, $save), function (index, value) {
+        values.push(value.innerHTML);
+    });
+    return values;
 }
