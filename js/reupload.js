89e9c9e27b5ab1a3dbea07e8a0dbdba923ff7d06
Xuhao Wang <wxuhao@gmail.com>
2021-04-06 23:04:41 -0400

    reupload unnecessary file

--- js/reupload.js
+++ js/reupload.js
@@ -2,18 +2,6 @@
 // store a reference to our file handle
 let fileHandle;
 
-async function getFile() {
-    // open file picker
-    [fileHandle] = await window.showOpenFilePicker();
-
-    if (fileHandle.type === 'file') {
-        // run file code
-    } else if (fileHandle.type === 'directory') {
-        // run directory code
-    }
-
-}
-
 async function getTheFile() {
     // open file picker
     [fileHandle] = await window.showOpenFilePicker();
@@ -22,6 +10,6 @@ async function getTheFile() {
         const fileData = await fileHandle.getFile();
         parseFile(fileData);
         // sleep
-        await new Promise(r => setTimeout(r, 500000));
+        await new Promise(r => setTimeout(r, 1000));
     }
 }
